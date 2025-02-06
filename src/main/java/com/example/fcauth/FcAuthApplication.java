a90fcb65e6afe7de4d0ba73f796f2fc3df324988
himchan92 <omea92@naver.com>
2025-02-06 13:02:49 +0900

    init commit

--- src/main/java/com/example/fcauth/FcAuthApplication.java
+++ src/main/java/com/example/fcauth/FcAuthApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FcAuthApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(FcAuthApplication.class, args);
     }
